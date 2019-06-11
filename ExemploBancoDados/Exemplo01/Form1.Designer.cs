51ce287eca25b8cccb7f76ccdb524cbfc3f8263c
HenriqueLinharesMontibeller <iquemontibeller@gmail.com>
2019-06-11 11:22:41 -0300

    Desenvolvemento da tela de cadastro de carro

---
+++ ExemploBancoDados/Exemplo01/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Exemplo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblCor = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.cbCor = new System.Windows.Forms.ComboBox();
+            this.mtbPreco = new System.Windows.Forms.MaskedTextBox();
+            this.nudAno = new System.Windows.Forms.NumericUpDown();
+            this.lblAno = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAno)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModelo.Location = new System.Drawing.Point(11, 9);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(61, 20);
+            this.lblModelo.TabIndex = 0;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // lblCor
+            // 
+            this.lblCor.AutoSize = true;
+            this.lblCor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCor.Location = new System.Drawing.Point(16, 67);
+            this.lblCor.Name = "lblCor";
+            this.lblCor.Size = new System.Drawing.Size(34, 20);
+            this.lblCor.TabIndex = 1;
+            this.lblCor.Text = "Cor";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreco.Location = new System.Drawing.Point(12, 132);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(50, 20);
+            this.lblPreco.TabIndex = 2;
+            this.lblPreco.Text = "Preço";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtModelo.Location = new System.Drawing.Point(15, 38);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(121, 26);
+            this.txtModelo.TabIndex = 3;
+            // 
+            // cbCor
+            // 
+            this.cbCor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCor.FormattingEnabled = true;
+            this.cbCor.Location = new System.Drawing.Point(16, 90);
+            this.cbCor.Name = "cbCor";
+            this.cbCor.Size = new System.Drawing.Size(121, 28);
+            this.cbCor.TabIndex = 4;
+            // 
+            // mtbPreco
+            // 
+            this.mtbPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbPreco.Location = new System.Drawing.Point(16, 164);
+            this.mtbPreco.Name = "mtbPreco";
+            this.mtbPreco.Size = new System.Drawing.Size(121, 26);
+            this.mtbPreco.TabIndex = 5;
+            // 
+            // nudAno
+            // 
+            this.nudAno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudAno.Location = new System.Drawing.Point(16, 216);
+            this.nudAno.Name = "nudAno";
+            this.nudAno.Size = new System.Drawing.Size(123, 26);
+            this.nudAno.TabIndex = 6;
+            // 
+            // lblAno
+            // 
+            this.lblAno.AutoSize = true;
+            this.lblAno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAno.Location = new System.Drawing.Point(12, 193);
+            this.lblAno.Name = "lblAno";
+            this.lblAno.Size = new System.Drawing.Size(38, 20);
+            this.lblAno.TabIndex = 7;
+            this.lblAno.Text = "Ano";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(64, 248);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 34);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblAno);
+            this.Controls.Add(this.nudAno);
+            this.Controls.Add(this.mtbPreco);
+            this.Controls.Add(this.cbCor);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lblCor);
+            this.Controls.Add(this.lblModelo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAno)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblCor;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.ComboBox cbCor;
+        private System.Windows.Forms.MaskedTextBox mtbPreco;
+        private System.Windows.Forms.NumericUpDown nudAno;
+        private System.Windows.Forms.Label lblAno;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
